911a29db97e027fa8b4470a8009cf16936851035
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-28 13:15:11 -0400

    add fetch team and on load functions

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const teamsContainer = document.querySelector('.teams-container')
 const teamsForm = document.getElementById('team-form')
 const BASEURL = "http://localhost:3000/teams"
 let newTeam = false
+
+// 1) On Load
+document.addEventListener('DOMContentLoaded', () => {
+    const addTeamButton = document.getElementById('add-team-button');
+    addTeamButton.addEventListener('click', handleAddTeamButton)
+    fetchTeams();
+})
+
+// 1) fetch teams
+function fetchTeams() {
+    fetch(BASEURL)
+    .then(response => response.json())
+    .then(addTeamsToDOM)
+}
