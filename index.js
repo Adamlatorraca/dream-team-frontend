52c323bd1d544a05b1fa5e1719466386b8aa0e0e
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-28 13:13:59 -0400

    add core definitions

---
+++ index.js
@@ -0,0 +1,4 @@
+const teamsContainer = document.querySelector('.teams-container')
+const teamsForm = document.getElementById('team-form')
+const BASEURL = "http://localhost:3000/teams"
+let newTeam = false
