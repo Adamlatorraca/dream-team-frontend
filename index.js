23c12d5ad42c5f8755e0bf8e112a3719e1ed982c
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-28 13:18:02 -0400

    add new team form

--- index.js
+++ index.js
@@ -16,3 +16,42 @@ function fetchTeams() {
     .then(response => response.json())
     .then(addTeamsToDOM)
 }
+
+// 2) New Team form
+function addTeamFormToDOM() {
+    const teamsForm = document.getElementById('team-form')
+    teamsForm.innerHTML +=
+        `<input type="text" name="name" value="" placeholder="Liquid, Astralis, NaVi, etc." class="input-team-name">
+        <br>
+        <input type="text" name="logo" value="" placeholder="imgur.com/linkyourlogo" class="input-team-logo">
+        <br>
+        <br>
+        <input type="text" name="player1" value="" placeholder="IGL" class="input-team-igl">
+        <br>
+        <input type="text" name="player2" value="" placeholder="Entry" class="input-team-entry">
+        <br>
+        <input type="text" name="player3" value="" placeholder="Lurker" class="input-team-lurker">
+        <br>
+        <input type="text" name="player4" value="" placeholder="Support" class="input-team-support">
+        <br>
+        <input type="text" name="player5" value="" placeholder="AWP" class="input-team-awp">
+        <br>
+        <input id="submit" type="submit" name="submit" value="Submit">`
+}
+
+// 3) Handle add new team button
+function handleAddTeamButton(e) {
+    const teamsForm = document.getElementById('team-form')
+    const addTeamButton = document.getElementById('add-team-button');
+    addTeamButton.addEventListener('click', () => {
+    newTeam = !newTeam
+    if (newTeam) {
+        teamsForm.style.display = 'block'
+        teamsForm.addEventListener('submit', event => {
+            handleTeamFormSubmit(event)
+        })
+    } else {
+        teamsForm.style.display = 'none'
+    }
+})
+}
