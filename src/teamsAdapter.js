8eccf5a3d37e9ef1f55881dbb19ea5b508e0a513
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-10 11:20:36 -0400

    add teams adapter

---
+++ src/teamsAdapter.js
@@ -0,0 +1,99 @@
+// communicate with back end
+
+class TeamsAdapter {
+    constructor(){
+        this.baseUrl = "http://localhost:3000/teams"
+    }
+
+    fetchTeams(){
+        fetch(this.baseUrl)
+        .then(res => res.json())
+        .then(json => {
+            json.data.forEach((el)=>{
+                let team = new Team(el.attributes)
+                team.attachToDom()
+            })
+        })
+    }
+
+    sendPatchRequest(teamId) {
+        const name = document.getElementById(`update-name-${teamId}`).value
+        const logo = document.getElementById(`update-logo-${teamId}`).value
+
+        let teamObj = {
+            name: name,
+            logo: logo
+        }
+
+        let configObj = {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json",
+                "Accepts": "application/json"
+            },
+            body: JSON.stringify(teamObj)
+        }
+        fetch(this.baseUrl + `/${teamId}`, configObj)
+        .then(res => res.json())
+        .then(response => {
+            let team = Team.all.find((i) => i.id === response.data.attributes.id)
+            team.updateTeamOnDom(response.data.attributes)
+        })
+
+        let form = document.getElementById(`update-form-${teamId}`)
+        form.remove()
+    }
+
+    createTeam(e){
+        e.preventDefault()
+
+
+        const name = document.getElementById('team-name').value
+        const logo = document.getElementById('team-logo').value
+
+        let newTeamObj = {
+            name,
+            logo
+        }
+
+        let configObj = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(newTeamObj)
+        }
+
+        //pessimistic rendering (we don't attach to DOM until we get a response back)
+        fetch('http://localhost:3000/teams', configObj)
+            .then(res => res.json())
+            .then(json => {
+                let team = new Team(json.data.attributes)
+                team.attachToDom()
+            })
+        teamForm.reset()
+        //const newForm = document.getElementById('new-form-container')
+        //newForm.hidden = true
+        //const newFormBtn = document.getElementById('new-form-btn')
+        //newFormBtn.hidden = false
+    }
+
+        // DELETE
+        deleteTeam(id){
+            // remove from db
+                let configObj = {
+                    method: 'DELETE',
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json"
+                    }
+                }
+        
+                fetch(`http://localhost:3000/teams/${id}`, configObj)
+                .then(res => res.json())
+                .then(json => {
+                    alert(json.message)
+                })
+            }
+}
